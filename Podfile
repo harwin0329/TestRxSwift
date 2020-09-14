b1ba9e9fdfab86bbb49e06f00473027f53335b7d
434790913@qq.com <434790913@qq.com>
2020-09-14 15:20:20 +0800

    ２０２０年０９月１４日１５：２０：１６

---
+++ Podfile
@@ -0,0 +1,6 @@
+use_frameworks!
+
+target 'TestRxSwift' do
+    pod 'RxSwift', '~> 5'
+    pod 'RxCocoa', '~> 5'
+end
--- TestRxSwift/ViewController.swift
+++ TestRxSwift/ViewController.swift
@@ -6,15 +6,111 @@
 //  Copyright © 2020 WoQi. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
-class ViewController: UIViewController {
+/*
+ 负责发送事件(Event)
+ Observeable
 
+ 负责订阅Observeable，监听Observeable发送的时间（Event）
+ Observer
+
+ */
+
+enum MyError: Error {
+    case MyError1
+}
+
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+//        let v = UILabel(frame: CGRect(x: 0, y: 200, width: 100, height: 30))
+//        view.addSubview(v)
+//
+////        let observable = Observable<Int>.create { observer in
+////            observer.onNext(00)
+////            observer.onNext(011)
+////            observer.onError(MyError.MyError1)
+////            observer.onNext(22)
+////            observer.onCompleted()
+////            observer.onNext(33)
+////            return Disposables.create()
+////        }
+//
+////        let observable = Observable.just(1)
+////        let observable = Observable.of(1,2,3)
+////        let observable = Observable.from([1, 2, 3])
+////        let observable = Observable.just([1, 2, 3])
+//
+////        DispatchTimeInterval
+//        let observable = Observable<Int>.timer(.seconds(3), period: .seconds(1), scheduler: MainScheduler.instance)
+//
+////        observable.subscribe { event in
+////            switch event {
+////            case let .next(element):
+////                print("next", element)
+////            case let .error(error):
+////                print("error", error)
+////            case .completed:
+////                print("completed")
+////            }
+////        }
+//
+////        observable.map { "\($0)" }.bind(to: v.rx.text)
+//
+//        let disposed = observable.subscribe(onNext: { i in
+//            print(i)
+//        }, onError: { error in
+//            print(error)
+//        }, onCompleted: {
+//            print("onCompleted")
+//        }, onDisposed: {
+//            print("onDisposed")
+//        })
+//
+////        disposed.dispose()
+////        take
+//
+//        let binder = Binder<String>(v) { v, value in
+//            v.text = value
+//        }
+////        observable.subscribe(binder).
+//
+//        observable.take(1).subscribe(onNext: { i in
+//            print("A+\(i)")
+//        }, onError: { error in
+//            print(error)
+//        }, onCompleted: {
+//            print("onCompleted")
+//        }, onDisposed: {
+//            print("onDisposed")
+//        })
+//
+//        PublishSubject<String>.init()
+//
+//        let b = BehaviorSubject(value: 11)
+//
+//        let c = ReplaySubject<String>.create(bufferSize: 2)
+////        c.subscribe(<#T##on: (Event<String>) -> Void##(Event<String>) -> Void#>)
+        
+        
+        let d = Variable("11111")
+        d.value = "2222"
+        d.asObservable().subscribe { (event) in
+            print("第一次",event)
+        }
+        d.value = "333"
+        
+        
+        d.asObservable().subscribe { (event) in
+            print("第二次",event)
+        }
+        d.value = "444"
+
+
     }
-
-
 }
-
